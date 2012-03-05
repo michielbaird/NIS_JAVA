eacf8721e8eda2325751c49e5a22d8a32fdff93d
Henk Joubert <jouberthenk@gmail.com>
2012-03-05 10:53:57 +0200

    test commit

--- src/com/nis/server/Server.java
+++ src/com/nis/server/Server.java
@@ -1,5 +1,14 @@
 package com.nis.server;
 
+import java.util.HashMap;
+
 public class Server {
+	private static HashMap<String, String> keyMap;
+	
+	public static void main(String [] argv) {
+		//Initialise Key Map;
+		keyMap = new HashMap<String, String>();
+		
+	}
 
 }
