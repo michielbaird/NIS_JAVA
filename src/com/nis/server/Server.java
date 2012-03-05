6b9433c13e7a5e5c84608896358ed66fe5ef7799
Michiel Baird <michielbaird@gmail.com>
2012-03-05 12:11:00 +0200

    test commit

--- src/com/nis/server/Server.java
+++ src/com/nis/server/Server.java
@@ -6,12 +6,12 @@ import java.util.HashMap;
 
 
 public class Server  {
-	//private static HashMap<String, String> keyMap;
+	private static HashMap<String, String> keyMap;
 	private static ServerSocket incomingSocket;
 	
 	public static void main(String [] argv) {
 		//Initialise Key Map;
-		//keyMap = new HashMap<String, String>();
+		keyMap = new HashMap<String, String>();
 		try {
 			incomingSocket = new ServerSocket(8081);
 		} catch (Exception e) {
