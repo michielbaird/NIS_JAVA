a09af417f09c5793f87fd08066c42768cda42d3e
Henk Joubert <jouberthenk@gmail.com>
2012-03-05 11:31:58 +0200

    server structure

---
+++ src/com/nis/client/Client.java
@@ -0,0 +1,23 @@
+package com.nis.client;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class Client {
+	public static void main(String argv[]) throws Exception
+	 {
+	  String sentence;
+	  String modifiedSentence;
+	  BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
+	  Socket clientSocket = new Socket("localhost", 8081);
+	  DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+	  BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+	  sentence = inFromUser.readLine();
+	  outToServer.writeBytes(sentence + '\n');
+	  modifiedSentence = inFromServer.readLine();
+	  System.out.println("FROM SERVER: " + modifiedSentence);
+	  clientSocket.close();
+	 }
+}
--- src/com/nis/server/Server.java
+++ src/com/nis/server/Server.java
@@ -1,14 +1,31 @@
 package com.nis.server;
 
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.HashMap;
 
-public class Server {
-	private static HashMap<String, String> keyMap;
+
+public class Server  {
+	//private static HashMap<String, String> keyMap;
+	private static ServerSocket incomingSocket;
 	
 	public static void main(String [] argv) {
 		//Initialise Key Map;
-		keyMap = new HashMap<String, String>();
-		
+		//keyMap = new HashMap<String, String>();
+		try {
+			incomingSocket = new ServerSocket(8081);
+		} catch (Exception e) {
+			
+		}
+		while (true) {
+			try {
+				Socket clientSocket =  incomingSocket.accept();
+				SocketHandler handler =  new SocketHandler(clientSocket);
+				handler.start();
+			} catch (Exception e) {
+				
+			}
+		}
 	}
 
 }
