865802159e6ff0a416ca9dec143563dd9a34a537
Michiel Baird <michielbaird@gmail.com>
2012-03-07 13:35:09 +0200

    added user map

---
+++ src/com/nis/server/ServerInfo.java
@@ -0,0 +1,31 @@
+package com.nis.server;
+
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+
+import com.google.gson.Gson;
+
+public class ServerInfo {
+	private final HashMap<String, InetSocketAddress> userList;
+	private final Gson gson;
+	
+	public ServerInfo() {
+		userList =  new HashMap<String, InetSocketAddress>();
+		gson = new Gson();
+	}
+	
+	public void removeUser(String handle) {
+		if (userList.containsValue(handle)) {
+			userList.remove(handle);
+		}
+	}
+	
+	public void addUser(String handle, InetSocketAddress socketAddress) {
+		userList.put(handle, socketAddress);
+	}
+	
+	public String getUserListJson() {
+		return gson.toJson(userList);
+	}
+
+}
