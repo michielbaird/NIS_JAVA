49fdc815422072872f5c81a0882e9ddf53cf99c5
Michiel Baird <michielbaird@gmail.com>
2012-03-13 07:06:40 +0200

    message handler

---
+++ src/com/nis/shared/requests/Message.java
@@ -0,0 +1,8 @@
+package com.nis.shared.requests;
+
+public class Message {
+	public String encryptedMessage;
+	public Message(String encryptedMessage) {
+		this.encryptedMessage = encryptedMessage;
+	}
+}
